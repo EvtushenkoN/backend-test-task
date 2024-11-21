e5f92e4a95ce02e9f1af6e62c03ecbcf7d43a800
Nikita Evtushenko <nevtushenko@psystems.biz>
2024-11-21 17:48:26 +0500

    TestTask: Initial commit - installed packages, created autogenerated entities

---
+++ src/Entity/Discount.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DiscountRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DiscountRepository::class)]
+class Discount
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $code = null;
+
+    #[ORM\Column]
+    private ?bool $type = null;
+
+    #[ORM\Column]
+    private ?float $discount = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): static
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getType(): ?bool
+    {
+        return $this->type;
+    }
+
+    public function setType(bool $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getDiscount(): ?float
+    {
+        return $this->discount;
+    }
+
+    public function setDiscount(float $discount): static
+    {
+        $this->discount = $discount;
+
+        return $this;
+    }
+}
